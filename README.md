95a81feaf466e43b9f2f491cd401504e8de339c5
Gabe Salvania <bsalvania@ucsed.edu>
2023-05-26 20:52:34 -0700

    Done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Lab 8 - Starter
+
+1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+   1. Within a Github action that runs whenever code is pushed
+   2. Manually run them locally before pushing code
+   3. Run them all after all development is completed
+   - For automated tests, I would choose the first option because it is the only one out of the 3 that will actually automate the tests and will also test your software as it is being developed. The second option will run manually and so will the third. Furthermore, the third one doesn't test your software as it is being developed but after its done, so the software will most likely have bugs and errors.
+2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+    - No, I would make a unit-test for that. An end to end test is for emulating user input.
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   - I would not because the "message" feature depends on user input and most likely relies on other components (like the other user receiving the message), so it would be better to test it using end to end testing. 
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+   - I would because this feature can easily be checked and tested with using a unit test. It doesn't rely on other components because we are just checking the length of the message.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,88 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// Tests for isPhoneNumber: first 2 positives then 2 negatives
+test('123-456-7890 is a valid phone number', () => {
+    expect(functions.isPhoneNumber("123-456-7890")).toBe(true);
+});
+
+test('(123)456-7890 is a valid phone number', () => {
+    expect(functions.isPhoneNumber("(123)456-7890")).toBe(true);
+});
+
+test('0987-654-321 is a valid phone number', () => {
+    expect(functions.isPhoneNumber("0987-654-321")).toBe(false);
+});
+
+test('666 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber("666")).toBe(false);
+});
+
+// Tests for isEmail: first 2 positives then 2 negatives
+test('bsalvania@ucsd.edu is a valid email', () => {
+    expect(functions.isEmail("bsalvania@ucsd.edu")).toBe(true);
+});
+
+test('coolBeans@gmail.com is a valid email', () => {
+    expect(functions.isEmail("coolBeans@gmail.com")).toBe(true);
+});
+
+test('coolBeans@@gmail.com is not a valid email', () => {
+    expect(functions.isEmail("coolBeans@@gmail.com")).toBe(false);
+});
+
+test('coolBeans@gmailcom is not a valid email', () => {
+    expect(functions.isEmail("coolBeans@gmailcom")).toBe(false);
+});
+
+// Tests for isStrongPassword: first 2 positives then 2 negatives
+test('a123 is a strong password', () => {
+    expect(functions.isStrongPassword("a123")).toBe(true);
+});
+
+test('pLrc94BE5rb is a strong password', () => {
+    expect(functions.isStrongPassword("pLrc94BE5rb")).toBe(true);
+});
+
+test('123 is not a strong password', () => {
+    expect(functions.isStrongPassword("123")).toBe(false);
+});
+
+test('superDuperLongPassword is not a strong password', () => {
+    expect(functions.isStrongPassword("superDuperLongPassword")).toBe(false);
+});
+
+// Tests for isDate: first 2 positives then 2 negatives
+test('05/26/2023 is a valid date', () => {
+    expect(functions.isDate("05/26/2023")).toBe(true);
+});
+
+test('5/9/2023 is a valid date', () => {
+    expect(functions.isDate("5/9/2023")).toBe(true);
+});
+
+test('05/26/23 is not a valid date', () => {
+    expect(functions.isDate("05/26/23")).toBe(false);
+});
+
+test('5/9/23 is not a valid date', () => {
+    expect(functions.isDate("5/9/23")).toBe(false);
+});
+
+// Tests for isHexColor: first 2 positives then 2 negatives
+test('00FF00 is a valid hex color', () => {
+    expect(functions.isHexColor("00FF00")).toBe(true);
+});
+
+test('#800080 is a valid hex color', () => {
+    expect(functions.isHexColor("#800080")).toBe(true);
+});
+
+test('#green is not a valid hex color', () => {
+    expect(functions.isHexColor("#green")).toBe(false);
+});
+
+test('80008 is not a valid hex color', () => {
+    expect(functions.isHexColor("80008")).toBe(false);
+});
